2dc8e1b647cb8510284116903476d217dce1eba1
Jacob Shafer <jacshfr@gmail.com>
2015-04-23 15:36:34 -0700

    replaces ga-tracking with gtm

--- media/js/privacy/privacy-day.js
+++ media/js/privacy/privacy-day.js
@@ -8,6 +8,7 @@ $(function() {
     var pager = Mozilla.Pager.pagers[0];
     var $documentRoot = $(document);
     var $firstPager = $('.pager').first();
+    var href;
 
     // scroll to top of pager when switching tabs so all
     // tab content is visible after switching tabs
@@ -29,71 +30,68 @@ $(function() {
     // enable sticky tab nagivation
     $('#button-nav-wrapper').waypoint('sticky');
 
-    var trackClick = function (gaArgs, href, event) {
-        window.dataLayer = window.dataLayer || [];
-        if (event.metaKey || event.ctrlKey) {
-            // Open link in new tab
-            window.gaTrack(gaArgs);
-        } else {
-            event.preventDefault();
-            window.gaTrack(gaArgs, function() { window.location = href; });
-        }
-    };
-
     // Setup GA tracking for main tabs
     $('#tips-nav-direct a').on('click', function() {
         var thisTabName = $(this).attr('href');
         console.log(thisTabName);
-        window.gaTrack([
-            '_trackEvent',
-            '/privacy/ Interactions',
-            thisTabName.substr(1), // strip # char from tab hash
-            'Tab Click'
-        ]);
+        window.dataLayer = window.dataLayer || [];
+        window.dataLayer.push({
+            event: 'privacy-interactions',
+            browserAction: thisTabName.substr(1),
+            interaction: 'Tab Click'
+        });
     });
 
     // Setup GA tracking for paragraph and list links
     $('.tip-column p a, .tip-column li a').on('click', function(e) {
-        trackClick([
-            '_trackEvent',
-            '/privacy/ Interactions',
-            pager.currentPage.id,
-            $(this).attr('href')
-        ], $(this).attr('href'), e);
+        href = $(this).attr('href');
+        window.dataLayer = window.dataLayer || [];
+        window.dataLayer.push({
+            event: 'privacy-interactions',
+            browserAction: pager.currentPage.id,
+            interaction: $(this).attr('href'),
+            eventCallback: function() {
+                window.location = href;
+            }
+        });
     });
 
     // Setup GA tracking TED video link
     $('.greenwald a').on('click', function(e) {
-        trackClick([
-            '_trackEvent',
-            '/privacy/ Interactions',
-            pager.currentPage.id,
-            'Why Privacy Matters CTA Btn'
-        ], $(this).attr('href'), e);
+        href = $(this).attr('href');
+        window.dataLayer = window.dataLayer || [];
+        window.dataLayer.push({
+            event: 'privacy-interactions',
+            browserAction: pager.currentPage.id,
+            interaction: 'Why Privacy Matters CTA Btn',
+            eventCallback: function() {
+                window.location = href;
+            }
+        });
     });
 
     // Setup GA tracking for next tab buttons
     $('.tip-footer .next a').on('click', function(e) {
         e.preventDefault();
         pager.nextPageWithAnimation();
-        window.gaTrack([
-            '_trackEvent',
-            '/privacy/ Interactions',
-            pager.currentPage.id,
-            'Next'
-        ]);
+        window.dataLayer = window.dataLayer || [];
+        window.dataLayer.push({
+            event: 'privacy-interactions',
+            browserAction: pager.currentPage.id,
+            interaction: 'Next'
+        });
     });
 
     // Setup GA tracking for previous tab buttons
     $('.tip-footer .previous a').on('click', function(e) {
         e.preventDefault();
         pager.prevPageWithAnimation();
-        window.gaTrack([
-            '_trackEvent',
-            '/privacy/ Interactions',
-            pager.currentPage.id,
-            'Previous'
-        ]);
+        window.dataLayer = window.dataLayer || [];
+        window.dataLayer.push({
+            event: 'privacy-interactions',
+            browserAction: pager.currentPage.id,
+            interaction: 'Previous'
+        });
     });
 
 });
